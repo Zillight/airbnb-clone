542503967022fc5f1fc27a469fb8892cd3d355bb
Banji Zillight <banjiolawolez@gmail.com>
2023-04-15 19:23:55 +0100

    created Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Hero(){
+    return(
+        <section>
+            <img src={require("../images/photo-grid.png")} alt="photo-grid"/>
+            <h1></h1>
+        </section>
+    )
+}
