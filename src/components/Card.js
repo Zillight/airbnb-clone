3231e2498ad107bee09bfac7e5f09caa7df67e9c
Banji Zillight <banjiolawolez@gmail.com>
2023-04-16 00:17:13 +0100

    hard coding data into  card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,17 @@
 import React from "react"
 
 export default function Card(props) {
-    
+    return(
+        <div>
+            <img src={require("../images/katie-zaferes.png")} alt="athlete"/>
+            <div>
+                <img src={require("../images/star.png")} alt="rating-star" />
+                <span>(6) • USA</span>
+                <p>Life lessons with Katie Zaferes</p>
+                <p><span>from $136</span> / person</p>
+            </div>
+        </div>
+    )
 }
 
 
