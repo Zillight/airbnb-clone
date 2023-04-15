63d7202e7f942df489017be9e45a5ddcbfb18ee5
Banji Zillight <banjiolawolez@gmail.com>
2023-04-16 00:28:14 +0100

    styling card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,13 @@ import React from "react"
 
 export default function Card(props) {
     return(
-        <div>
-            <img src={require("../images/katie-zaferes.png")} alt="athlete"/>
+        <div className="card">
+            <img src={require("../images/katie-zaferes.png")} alt="athlete" className="card--img"/>
             <div>
-                <img src={require("../images/star.png")} alt="rating-star" />
+                <img src={require("../images/star.png")} alt="rating-star" className="card--star"/>
                 <span>(6) • USA</span>
+            </div>
+            <div>
                 <p>Life lessons with Katie Zaferes</p>
                 <p><span>from $136</span> / person</p>
             </div>
