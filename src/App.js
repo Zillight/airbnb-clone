4ce50e086025cc5348400171c8e4b0b72027cc1d
Banji Zillight <banjiolawolez@gmail.com>
2023-04-15 23:47:40 +0100

    card component not working

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
-import data from "../data";
+// import data from "./data";
 
 function App() {
+
   return (
-    <div className="App">
+    <div>
       <Navbar />
       <Hero />
       <Card />
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,22 @@
 import React from "react"
 
-export default function Card(props){
-    return(
-        <div>
-            <img src={require(`../images/${props.CoverImg}`)} alt="" />
-            <div>
-                <img src="../images/star.png" />
-            </div>
-        </div>
-    )
+export default function Card(props) {
+    
 }
+
+
+
+
+
+
+
+// return(
+//     <div>
+//         <img src={require(`../images/`)} alt="" />
+//         <div>
+//             <img src="../images/star.png" />
+//             <span>{props.stats.rating}</span>
+//             <span>{props.title}</span>
+//         </div>
+//     </div>
+// )
