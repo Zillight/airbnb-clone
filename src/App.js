a034560a5af528b2a2e49ee6b6745337c9307266
Banji Zillight <banjiolawolez@gmail.com>
2023-04-15 20:04:06 +0100

    import data in App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "../data";
 
 function App() {
   return (
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,10 @@ import React from "react"
 export default function Card(props){
     return(
         <div>
-            
+            <img src={require(`../images/${props.CoverImg}`)} alt="" />
+            <div>
+                <img src="../images/star.png" />
+            </div>
         </div>
     )
 }
