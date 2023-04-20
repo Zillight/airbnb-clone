24a5a8134a2d94b6545033c16a6cfb6a1138ff31
Banji Zillight <banjiolawolez@gmail.com>
2023-04-20 23:57:14 +0100

    mapping the cards

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import Slides from "./data"
 // import Katie from "../src/images/katie-zaferes.png"
 // import {useState} from "react"
 // import data from "./data";
 
 function App() {
-
     // const [count, setCount] = useState(0)
+    const cardSlides = Slides.map(Slide => )
   return (
     <div>
       <Navbar />
       <Hero />
       <div className="app--card">
+        <Card 
+          coverImg="katie-zaferes.png"
+          rating="5.0"
+          location="USA"
+          price="136"
+          title="Life lessons with Katie Zaferes"
+        />
         <Card 
           coverImg="katie-zaferes.png"
           rating="5.0"
