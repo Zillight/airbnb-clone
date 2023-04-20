e482b1b149128f7c05978fbe5319a3898674daa8
Banji Zillight <banjiolawolez@gmail.com>
2023-04-21 00:10:58 +0100

    mapping through data.js

--- src/App.js
+++ src/App.js
@@ -2,47 +2,31 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
-import Slides from "./data"
+import data from "./data"
 // import Katie from "../src/images/katie-zaferes.png"
 // import {useState} from "react"
 // import data from "./data";
 
 function App() {
     // const [count, setCount] = useState(0)
-    const cardSlides = Slides.map(Slide => )
+    const cards = data.map(item => {
+      return(
+        <Card
+        coverImg={item.coverImg}
+        rating={item.stats.rating}
+        location={item.location}
+        price={item.price}
+        title={item.title}
+        />
+      )
+    })
+
   return (
     <div>
       <Navbar />
       <Hero />
-      <div className="app--card">
-        <Card 
-          coverImg="katie-zaferes.png"
-          rating="5.0"
-          location="USA"
-          price="136"
-          title="Life lessons with Katie Zaferes"
-        />
-        <Card 
-          coverImg="katie-zaferes.png"
-          rating="5.0"
-          location="USA"
-          price="136"
-          title="Life lessons with Katie Zaferes"
-        />
-        <Card 
-          coverImg="wedding-photography.png"
-          rating="5.0"
-          location="Online"
-          price="125"
-          title="Learn Wedding Photography"
-        />
-        <Card 
-          coverImg="mountain-bike.png"
-          rating="4.8"
-          location="Norway"
-          price="50"
-          title="Group Mountain Biking"
-        />
+      <div className="app--card"> 
+        {cards}
       </div>
     </div>
   );
