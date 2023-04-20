d8b9ad69153280cb2d74081a6ce6b84a2639178e
Banji Zillight <banjiolawolez@gmail.com>
2023-04-20 23:11:02 +0100

    fixed... lessons learnt

--- src/App.js
+++ src/App.js
@@ -2,38 +2,25 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
-import {useState} from "react"
+// import Katie from "../src/images/katie-zaferes.png"
+// import {useState} from "react"
 // import data from "./data";
 
 function App() {
-  const planets = [
-    { name: "Mars", isGasPlanet: false }, 
-    { name: "Earth", isGasPlanet: false },
-    { name: "Jupiter", isGasPlanet: true }, 
-    { name: "Venus", isGasPlanet: false }, 
-    { name: "Neptune", isGasPlanet: true }, 
-    { name: "Uranus", isGasPlanet: true }
-    ];
-    const [count, setCount] = useState(0)
+
+    // const [count, setCount] = useState(0)
   return (
     <div>
       <Navbar />
       <Hero />
       <div className="app--card">
         <Card 
-          coverImg="../images/katie-zaferes.png"
+          coverImg="katie-zaferes.png"
           rating="6"
           location="USA"
           price="136"
           title="Life lessons with Katie Zaferes"
         />
-      </div>
-      {planets.map(planet => !planet.isGasPlanet && <h1>{planet.name}</h1>)}
-      <div>
-        <button onClick={()=>setCount(count + 1)}>Increase</button>
-        <button onClick={()=>setCount(count - 1)}>Decrease</button>
-        <button onClick={()=>setCount(0)}>Set to Zero</button>
-        {count}
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Card(props) {
     return(
         <div className="card">
-            <img src={props.coverImg} alt="athlete" className="card--img"/>
+            <img src={require("../images/"+ props.coverImg)} alt="athlete" className="card--img"/>
             <div className="card--stats">
                 <img src={require("../images/star.png")} alt="rating-star" className="card--star"/>
                 <span>({props.rating}) •</span>
@@ -16,15 +16,3 @@ export default function Card(props) {
         </div>
     )
 }
-
-
-// return(
-//     <div>
-//         <img src={require(`../images/`)} alt="" />
-//         <div>
-//             <img src="../images/star.png" />
-//             <span>{props.stats.rating}</span>
-//             <span>{props.title}</span>
-//         </div>
-//     </div>
-// )
